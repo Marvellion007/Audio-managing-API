84327689dc2db9e19eaefcb1265f83f345439709
Marvellion007 <shukla.ajitesh45@gmail.com>
2021-02-22 17:49:04 +0530

    chore: refactored post method in controller layer

--- core/controller/message.py
+++ core/controller/message.py
@@ -22,47 +22,48 @@ def get_db():
 @router.post("/{audioFileType}")
 def create_message(audioFileType: str, audioFileMetadata: dict, db: Session = Depends(get_db)):
     Message_obj = base.createbase(audioFileType = audioFileType, audioFileMetadata = audioFileMetadata )
-    result = message.get_message(db,audioFileType=audioFileType, audioFileID=audioFileMetadata["ID"])
+    result = message.get_message(db,audioFileType=audioFileType, audioFileID=audioFileMetadata["ID"]) 
     if result is not None:
+        given_date = audioFileMetadata["UPLOAD_TIME"]
+        date_format = "%Y-%m-%d %H:%M:%S"
+        given = datetime.strptime(given_date, date_format)
+        now =  datetime.now()
+        if audioFileType.lower()=='"song"':
+            if len(audioFileMetadata["NAME"])>100:
+                raise HTTPException(status_code=400, detail="Name length exeeded")
+            if audioFileMetadata["DURATION"]<0:
+                raise HTTPException(status_code=400, detail="duration cannot be negetive")
+            if given<now:
+                raise HTTPException(status_code=400, detail="date cannot be in past")  
+        if audioFileType.lower()=='"podcast"':
+            if len(audioFileMetadata["NAME"])>100:
+                raise HTTPException(status_code=400, detail="Name length exeeded")
+            if audioFileMetadata["DURATION"]<0:
+                raise HTTPException(status_code=400, detail="duration cannot be negetive")
+            if "HOST" not in audioFileMetadata.keys():
+                raise HTTPException(status_code=400, detail="invalid request")
+            if len(audioFileMetadata["HOST"])>100:
+                raise HTTPException(status_code=400, detail="Host length exeeded")
+            if given<now:
+                raise HTTPException(status_code=400, detail="date cannot be in past")  
+            if audioFileMetadata["PARTICIPANTS"]:
+                if not isinstance(audioFileMetadata["PARTICIPANTS"], list):
+                    raise HTTPException(status_code=400, detail="participants should be of list type")
+        if audioFileType.lower()=='"audiobook"':
+            if len(audioFileMetadata["TITLE"])>100:
+                raise HTTPException(status_code=400, detail="Title length exeeded")
+            if len(audioFileMetadata["AUTHOR"])>100:
+                raise HTTPException(status_code=400, detail="Author length exeeded")
+            if len(audioFileMetadata["NARRATOR"])>100:
+                raise HTTPException(status_code=400, detail="Title length exeeded")
+            if audioFileMetadata["DURATION"]<0:
+                raise HTTPException(status_code=400, detail="duration cannot be negetive")
+            if given<now:
+                raise HTTPException(status_code=400, detail="date cannot be in past")  
+        result = message.create_message(db, Message_obj)
+        return result
+    else:
         raise HTTPException(status_code=400, detail="id already there")
-    given_date = audioFileMetadata["UPLOAD_TIME"]
-    date_format = "%Y-%m-%d %H:%M:%S"
-    given = datetime.strptime(given_date, date_format)
-    now =  datetime.now()
-    if audioFileType.lower()=='"song"':
-        if len(audioFileMetadata["NAME"])>100:
-            raise HTTPException(status_code=400, detail="Name length exeeded")
-        if audioFileMetadata["DURATION"]<0:
-            raise HTTPException(status_code=400, detail="duration cannot be negetive")
-        if given<now:
-            raise HTTPException(status_code=400, detail="date cannot be in past")  
-    if audioFileType.lower()=='"podcast"':
-        if len(audioFileMetadata["NAME"])>100:
-            raise HTTPException(status_code=400, detail="Name length exeeded")
-        if audioFileMetadata["DURATION"]<0:
-            raise HTTPException(status_code=400, detail="duration cannot be negetive")
-        if "HOST" not in audioFileMetadata.keys():
-            raise HTTPException(status_code=400, detail="invalid request")
-        if len(audioFileMetadata["HOST"])>100:
-            raise HTTPException(status_code=400, detail="Host length exeeded")
-        if given<now:
-            raise HTTPException(status_code=400, detail="date cannot be in past")  
-        if audioFileMetadata["PARTICIPANTS"]:
-            if not isinstance(audioFileMetadata["PARTICIPANTS"], list):
-                raise HTTPException(status_code=400, detail="participants should be of list type")
-    if audioFileType.lower()=='"audiobook"':
-        if len(audioFileMetadata["TITLE"])>100:
-            raise HTTPException(status_code=400, detail="Title length exeeded")
-        if len(audioFileMetadata["AUTHOR"])>100:
-            raise HTTPException(status_code=400, detail="Author length exeeded")
-        if len(audioFileMetadata["NARRATOR"])>100:
-            raise HTTPException(status_code=400, detail="Title length exeeded")
-        if audioFileMetadata["DURATION"]<0:
-            raise HTTPException(status_code=400, detail="duration cannot be negetive")
-        if given<now:
-            raise HTTPException(status_code=400, detail="date cannot be in past")  
-    result = message.create_message(db, Message_obj)
-    return result
 
 @router.get("/{audioFileType}/{audioFileID}")
 def read_message(audioFileType: str, audioFileID: int, db: Session = Depends(get_db)) :
